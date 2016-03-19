d340907cac721cd3f43f761aea542e6a8871e36b
Michael "Headchopperz" Haddon <dis_connected@ymail.com>
2016-03-19 05:28:31 +0000

    Entities can now animat

--- JS/Entity.js
+++ JS/Entity.js
@@ -1,15 +1,60 @@
+var ModelSet = [
+    [0, 1]
+];
+
+var _entity = {
+    Description: "none found",
+    X: 0,
+    Y: 0,
+    Z: 0,
+    old_X: 0,
+    old_Y: 0,
+    ModelSet: 0,
+    CurrentModel: 0,
+    LastModelUpdate: 0,
+    AnimationSpeed: 25,
+    Physics: {
+        On: false,
+        Collision: false,
+        Speed: 1
+    }
+}
+
+if (typeof Entities === "undefined") {
+    var Entities = new array();
+} else {
+    /**
+     * Buttons loaded from JSON will not have any of the default functions set
+     */
+    Entities.forEach(function(e) {
+        e._onDeath = _entity._onDeath;
+    });
+}
+
 function drawEntities(dt) {
-    var X = 100;
-    var Y = 100;
-    for (var Ey = 0; Ey < Models[0].length; Ey++) {
-        for (var Ex = 0; Ex < Models[0][Ey].length; Ex++) {
-            var e = Models[0][Ey][Ex];
-            if (e !== null) {
-                context.beginPath();
-                context.fillStyle = e;
-                context.rect(X + (Ex * 5), Y + (Ey * 5), 5, 5);
-                context.fill();
+    var now = Date.now();
+    
+    Entities.forEach(function(e) {
+        
+        if (e.LastModelUpdate + e.AnimationSpeed < now) {
+            if (++e.CurrentModel > ModelSet[e.ModelSet].length - 1) {
+                e.CurrentModel = 0;
+            }
+            e.LastModelUpdate = now;
+        }
+        
+        var ModelID = ModelSet[e.ModelSet][e.CurrentModel];
+        
+        for (var Ey = 0; Ey < Models[ModelID].length; Ey++) {
+            for (var Ex = 0; Ex < Models[ModelID][Ey].length; Ex++) {
+                var ModelInfo = Models[ModelID][Ey][Ex];
+                if (ModelInfo !== null) {
+                    context.beginPath();
+                    context.fillStyle = ModelInfo;
+                    context.rect(e.X + (Ex * 5), e.Y + (Ey * 5), 5, 5);
+                    context.fill();
+                }
             }
         }
-    }
+    });
 }
